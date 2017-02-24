448c5b370c6a225b1e510a37ff75d2b642d9a3f9
Charlie Steele <steelech@umich.edu>
2017-02-24 02:55:09 -0500

    clean up a bit

--- client/src/view/loginView.js
+++ client/src/view/loginView.js
@@ -1,10 +1,42 @@
 var loginView = {
 	renderLoginView: function() {
+		this.drawLoginContainer();
+		this.drawLoginHeader();
+		this.drawLoginForm();
+
+	}, 
+	drawLoginForm: function() {
+		var formContainer = document.createElement("div");
+		formContainer.id = "login-form-container";
+
+		var username = document.createElement("input");
+		username.id = "login-username-input";
+		username.type = "text";
+
+		var password = document.createElement("input");
+		password.id =  "login-password-input";
+		password.type = "password";
+
+		var submitButton = document.createElement("button");
+		submitButton.id = "login-submit-button";
+		submitButton.innerHTML = "Submit";
+
+		formContainer.appendChild(username);
+		formContainer.appendChild(password);
+		formContainer.appendChild(submitButton);
+		document.getElementById("login-container").appendChild(formContainer);
+
+	},
+	drawLoginContainer: function() {
+		var loginContainer = document.createElement("div");
+		loginContainer.id="login-container";
+		document.getElementsByTagName('body')[0].appendChild(loginContainer);
+	},
+	drawLoginHeader: function() {
 		var header = document.createElement("h1");
 		var headerText = document.createTextNode("Login");
 		header.appendChild(headerText);
-
-		document.getElementsByTagName('body')[0].appendChild(header);
+		document.getElementById("login-container").appendChild(header);
 	}
 }
 
--- es6-script.js
+++
@@ -1,2 +0,0 @@
-let name = 'World';
-let greeting = `Hello ${name}!`
