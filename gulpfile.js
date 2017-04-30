69495b260e6142623c05e6e7dcfac9de7a80302a
Charlie Steele <steelech@umich.edu>
2017-04-30 18:07:09 -0400

    gulp task for deploying a single file to s3

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict'
-
+var AWS = require('aws-sdk');
 var gulp = require('gulp');
 var fs = require('fs');
 var babel = require('gulp-babel');
@@ -7,6 +7,28 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
+
+ 
+gulp.task('deploy', () => {
+	AWS.config.loadFromPath('./aws.json');
+	fs.readFile('hello.html', function(err, data) {
+		if(err) { throw err; }
+		var s3 = new AWS.S3();
+	        s3.putObject({
+		        Bucket: 'pics-website',
+		        Key: 'hello.html',
+		        Body: data,
+			ACL: 'public-read',
+			ContentType: 'text/html',
+	        }, function(perr, pres) {
+			if(perr) {
+				console.log("error: ", perr);
+			} else {
+				console.log("success!");
+			}
+		});	
+	});
+});
 
 gulp.task('default', ['watch', 'es6']);
 
