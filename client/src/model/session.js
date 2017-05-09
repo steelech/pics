8a79ae0dfbc6de8505ce5ce4e8d7b89da7631eb9
Charlie Steele <steelech@umich.edu>
2017-05-09 03:09:39 -0400

    login works

--- client/src/model/session.js
+++ client/src/model/session.js
@@ -1,12 +1,14 @@
 import Api from '../../src/utils/api';
 var Session = {
 	login: function(username, password) {
-		console.log("logging in: ", username, password);
-		Api.sendRequestToBackend({}, "/login");
+		Api.sendRequestToBackend({username: username, password: password}, "/login", "POST").then(function(data) {
+			console.log("data: ", data);
+		});
 		// make ajax request
 		// use localStorage to store creds if successful 
 		// return 200 if successful, 404 + error message if not
-	}
+	},
+	
 }
 
 export default Session;
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,27 @@
 var Api = {
-	sendRequestToBackend: (data, endpoint) => {
-		console.log("sending data to backend");
+	sendRequestToBackend: (data, endpoint, method) => {
+		return new Promise((resolve, reject) => {
+			var request = new XMLHttpRequest();
+			request.open("POST", "http://localhost:8888/login", true);
+			request.setRequestHeader("Content-Type", "application/json");
+			request.send(JSON.stringify(data));
+
+			request.addEventListener("load", function(e) {
+				var responseStatus = e.target.status;
+				var responseData = JSON.parse(e.target.response);
+
+				var response = {
+					"responseStatus": responseStatus,
+					"responseData": responseData
+				}
+				resolve(response);
+			});
+			request.addEventListener("error", function(e) {
+				console.log("error!", e);
+			})
+
+		});
 	}
-}
+};
 
 export default Api;
