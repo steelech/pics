7c61227c260a2440495e85ea473058a6e97103bd
Charlie Steele <steelech@umich.edu>
2017-06-18 14:37:36 -0400

    send file uploads in chunks to avoid browser timeout

--- src/model/pics.js
+++ src/model/pics.js
@@ -4,17 +4,40 @@ export var Pic = {
 	}
 };
 
+var splitUpFiles = (files) => {
+	// split the files into arrays of length 30 
+	// this is needed so the browser doesn't timeout
+	var fileList = [];
+	var numChunks = Math.round(files.length / 30) + 1;
+	for(var i = 0;i < numChunks;i++) {
+		fileList.push(files.slice(i * 30, i * 30 + 30));
+	}
+
+	var count = 0;
+	var sendPics = (index) => {
+		if(index < numChunks) {
+			console.log('sending: ', fileList[index]);
+			var formData = new FormData();
+			for(let i = 0;i < fileList[index].length;i++) {
+				formData.append(fileList[index][i].name, fileList[index][i])
+			}
+			var xhr = new XMLHttpRequest();
+		  	xhr.open('POST', 'http://localhost:8888/pics', true);
+			xhr.send(formData);
+
+			xhr.onload = () => {
+				sendPics(index + 1);
+			}	
+		} else {
+			alert('done');
+		}
+	}	
+	sendPics(0);
+
+}
+
 export var Pics = {
 	send: function(pics) {
-		var formData = new FormData();
-		for(let i = 0;i < pics.length;i++) {
-			console.log(pics[i]);
-			formData.append(pics[i].name, pics[i]);
-		}
-		var xhr = new XMLHttpRequest;
-	  	xhr.open('POST', 'http://localhost:8888/pics', true);
-		xhr.send(formData);
-		console.log('formData: ', formData);
-
+		splitUpFiles(pics);
 	},
 };
