0cb4ff0b9ff1717a145152ae570bcf1aecda570e
Charlie Steele <steelech@umich.edu>
2017-06-18 22:52:48 -0400

    add albums button, move pics-index header buttons to top-right

--- src/components/base/pics/PicsHeader.js
+++ src/components/base/pics/PicsHeader.js
@@ -1,19 +1,28 @@
 var PicsHeader = {
 	render() {
 		console.log('rendering PicsHeader');
+		var buttonsContainer = document.createElement('div');
+		buttonsContainer.classList.add('buttons-container');
+		buttonsContainer.id = 'buttons-container';
+		this.container.appendChild(buttonsContainer);
 		var fileUploadButton = document.createElement('div');
 		fileUploadButton.classList.add('file-upload-button');
 		fileUploadButton.id = 'file-upload-button';
 		fileUploadButton.appendChild(document.createTextNode('+Pictures'));
 		fileUploadButton.addEventListener('click', this._handleUploadButtonClick.bind(this));
-		this.container.appendChild(fileUploadButton);
+		buttonsContainer.appendChild(fileUploadButton);
 		var albumCreateButton = document.createElement('div');
 		albumCreateButton.classList.add('add-album-button');
 		albumCreateButton.id = 'add-album-button';
 		albumCreateButton.appendChild(document.createTextNode('+Album'));
 		albumCreateButton.addEventListener('click', this._handleUploadButtonClick.bind(this));
-		this.container.appendChild(albumCreateButton);
-		this.container.appendChild(document.createTextNode('Pictures'));
+		buttonsContainer.appendChild(albumCreateButton);
+		this.container.appendChild(buttonsContainer);
+		var picsHeaderText = document.createElement('div');
+		picsHeaderText.classList.add('pics-header-text');
+		picsHeaderText.id = 'pics-header-text';
+		picsHeaderText.appendChild(document.createTextNode('Pictures'));
+		this.container.appendChild(picsHeaderText);
 	}
 };
 
