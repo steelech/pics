0e9cf067f332e379eb3e53ff277b74d549a6dcd9
Charlie Steele <steelech@umich.edu>
2017-08-24 16:06:16 -0400

    use pre-formatted thumbnails instead of having browser resize

--- src/components/base/pics/PicsList.js
+++ src/components/base/pics/PicsList.js
@@ -5,13 +5,10 @@ const PicsList = {
     const picsListContainer = document.createElement('div');
     picsListContainer.classList.add('pics-list');
     picsListContainer.id = 'pics-list';
-    picsListContainer.appendChild(document.createTextNode('Pics List'));
 
     pics.map((pic) => {
       let thisPic = document.createElement('img');
-      thisPic.src = pic.url;
-      thisPic.height = 100;
-      thisPic.width = 100;
+      thisPic.src = pic.thmbUrl;
       picsListContainer.appendChild(thisPic);
     })
     document.getElementById('pics-content').appendChild(picsListContainer);
