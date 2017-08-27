d56a07eeac2ebf5d6d0f3f97c54cc02032c2cf95
Charlie Steele <steelech@umich.edu>
2017-08-27 01:26:02 -0400

    start albums modal header/form

--- src/components/base/pics/albums/AlbumsModal.js
+++ src/components/base/pics/albums/AlbumsModal.js
@@ -1,13 +1,37 @@
 import Modal from 'components/ui/modal';
 
 const AlbumsModal = {
-  render({ container }) {
+  drawHeader({ container }) {
+    console.log('rendering AlbumsModal header');
+
+    const wrapper = document.createElement('div');
+    wrapper.id = 'albums-modal-header';
+    wrapper.classList.add('albums-modal-header');
+
+    const headerText = document.createElement('h1');
+    headerText.id = 'albums-modal-header-text';
+    headerText.classList.add('albums-modal-header-text');
+    headerText.appendChild(document.createTextNode('Albums Modal Header'));
+
+    wrapper.appendChild(headerText);
+    container.appendChild(wrapper);
+  },
+  drawForm({ container }) {
+    console.log('rendering AlbumsModal form');
+    const wrapper = document.createElement('div');
+    wrapper.id = 'albums-modal-form';
+    wrapper.classList.add('albums-modal-form');
+    wrapper.appendChild(document.createTextNode('Albums Modal Form'));
+    container.appendChild(wrapper);
+  },
+  render() {
     console.log('rendering albums modal');
-    const albumsModal = document.createElement('div');
-    albumsModal.id = 'albums-modal';
-    albumsModal.classList.add('albums-modal');
-    albumsModal.appendChild(document.createTextNode('Albums Modal'));
-    Modal.render(albumsModal);
+    const container = document.createElement('div');
+    container.id = 'albums-modal';
+    container.classList.add('albums-modal');
+    this.drawHeader({ container });
+    this.drawForm({ container });
+    Modal.render(container);
   },
 };
 
