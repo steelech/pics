7bfa6d0f3959c4d5c2a4437de7b657a89ca09179
Charlie Steele <steelech@umich.edu>
2017-08-29 01:56:57 -0400

    better styling for AlbumsRow/AlbumCard components

--- src/components/base/pics/albums/AlbumCard.js
+++ src/components/base/pics/albums/AlbumCard.js
@@ -1,13 +1,25 @@
 const AlbumCard = {
   render({ album, albumsRow, onClick }) {
+    const wrapper = document.createElement('div');
+    wrapper.id = 'album-card-container';
+    wrapper.classList.add('album-card-container');
+
     const albumCard = document.createElement('div');
     albumCard.id = 'album-card';
     albumCard.classList.add('album-card');
 
     const icon = document.createElement('i');
     icon.className = 'fa fa-folder';
+
+    const cardText = document.createElement('div');
+    cardText.classList.add('album-card-text');
+    cardText.appendChild(document.createTextNode(album.name));
+
+
     albumCard.appendChild(icon);
-    albumsRow.appendChild(albumCard);
+    albumCard.appendChild(cardText);
+    wrapper.appendChild(albumCard);
+    albumsRow.appendChild(wrapper);
     debugger
   },
 };
