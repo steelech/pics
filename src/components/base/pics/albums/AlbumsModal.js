ff01487b09652d951beaa9778c37f0827920e03f
Charlie Steele <steelech@umich.edu>
2017-12-13 00:01:56 -0500

    start styling for new AlbumsModal design

--- src/components/base/pics/albums/AlbumsModal.js
+++ src/components/base/pics/albums/AlbumsModal.js
@@ -1,6 +1,61 @@
 import Modal from 'components/ui/modal';
 import Albums from 'model/albums';
 
+const AlbumsModalHeader = () => {
+  const wrapper = document.createElement('div');
+  wrapper.id = 'albums-modal-header';
+  wrapper.classList.add('albums-modal-header');
+
+  const headerText = document.createElement('h1');
+  headerText.id = 'albums-modal-header-text';
+  headerText.classList.add('albums-modal-header-text');
+  headerText.appendChild(document.createTextNode('New Album'));
+
+  wrapper.appendChild(headerText);
+  return wrapper;
+};
+
+const AlbumsModalContent = {
+  render() {
+    const content = document.createElement('div');
+    content.id = 'albums-modal-content';
+    content.classList.add('albums-modal-content');
+    content.appendChild(document.createTextNode('Content'));
+
+    return content;
+  },
+};
+
+const AlbumsModalFooter = () => {
+  const footer = document.createElement('div');
+  footer.id = 'albums-modal-footer';
+  footer.classList.add('albums-modal-footer');
+
+  const cancelButtonWrapper = document.createElement('div');
+  cancelButtonWrapper.id = 'albums-modal-cancel-wrapper';
+  cancelButtonWrapper.classList.add('albums-modal-cancel-wrapper');
+
+  const cancelButton = document.createElement('div');
+  cancelButton.id = 'albums-modal-cancel-button';
+  cancelButton.classList.add('albums-modal-cancel-button');
+  cancelButton.appendChild(document.createTextNode('Cancel'));
+  cancelButtonWrapper.appendChild(cancelButton);
+
+  const createButtonWrapper = document.createElement('div');
+  createButtonWrapper.id = 'albums-modal-create-wrapper';
+  createButtonWrapper.classList.add('albums-modal-create-wrapper');
+
+  const createButton = document.createElement('div');
+  createButton.id = 'albums-modal-create-button';
+  createButton.classList.add('albums-modal-create-button');
+  createButton.appendChild(document.createTextNode('Create'));
+  createButtonWrapper.appendChild(createButton);
+
+  footer.appendChild(cancelButtonWrapper);
+  footer.appendChild(createButtonWrapper);
+  return footer;
+};
+
 const AlbumsModal = {
   tearDown() {
     document.body.removeChild(document.getElementById('modal'));
@@ -9,19 +64,6 @@ const AlbumsModal = {
     const albumName = document.getElementById('album-name-input-field').value;
     Albums.create(albumName).then(this.onSubmit);
     this.tearDown();
-  },
-  drawHeader({ container }) {
-    const wrapper = document.createElement('div');
-    wrapper.id = 'albums-modal-header';
-    wrapper.classList.add('albums-modal-header');
-
-    const headerText = document.createElement('h1');
-    headerText.id = 'albums-modal-header-text';
-    headerText.classList.add('albums-modal-header-text');
-    headerText.appendChild(document.createTextNode('New Album'));
-
-    wrapper.appendChild(headerText);
-    container.appendChild(wrapper);
   },
   drawAlbumNameInput({ wrapper }) {
     const albumNameInput = document.createElement('div');
@@ -36,6 +78,25 @@ const AlbumsModal = {
     albumNameInput.appendChild(albumNameInputField);
 
     wrapper.appendChild(albumNameInput);
+  },
+  drawPicCreateCheckbox({ wrapper }) {
+    const picCreateCheckboxWrapper = document.createElement('div');
+    picCreateCheckboxWrapper.id = 'pic-create-checkbox-wrapper';
+    picCreateCheckboxWrapper.classList.add('pic-create-checkbox-wrapper');
+
+    const addPicsCheckbox = document.createElement('input');
+    addPicsCheckbox.id = 'add-pics-checkbox';
+    addPicsCheckbox.classList.add('add-pics-checkbox');
+    addPicsCheckbox.type = 'checkbox';
+
+    const addPicsLabel = document.createElement('div');
+    addPicsLabel.id = 'add-pics-label';
+    addPicsLabel.classList.add('add-pics-label');
+    addPicsLabel.appendChild(document.createTextNode('Add images now'));
+
+    picCreateCheckboxWrapper.appendChild(addPicsCheckbox);
+    picCreateCheckboxWrapper.appendChild(addPicsLabel);
+    wrapper.appendChild(picCreateCheckboxWrapper);
   },
   drawSubmitButton({ wrapper }) {
     const albumSubmit = document.createElement('div');
@@ -63,6 +124,7 @@ const AlbumsModal = {
     wrapper.id = 'albums-modal-form';
     wrapper.classList.add('albums-modal-form');
     this.drawAlbumNameInput({ wrapper });
+    this.drawPicCreateCheckbox({ wrapper });
     this.drawSubmitButton({ wrapper });
     container.appendChild(wrapper);
   },
@@ -71,8 +133,15 @@ const AlbumsModal = {
     const container = document.createElement('div');
     container.id = 'albums-modal';
     container.classList.add('albums-modal');
-    this.drawHeader({ container });
-    this.drawForm({ container });
+
+    const header = AlbumsModalHeader();
+    const content = AlbumsModalContent.render();
+    const footer = AlbumsModalFooter();
+
+    container.appendChild(header);
+    container.appendChild(content);
+    container.appendChild(footer);
+
     Modal.render({ child: container });
   },
 };
